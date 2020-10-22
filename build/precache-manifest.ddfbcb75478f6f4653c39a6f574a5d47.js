86f07ddc473d454bc4dc53d4236fca9b2a696c28
Leandro Vieira <alotropico@gmail.com>
2020-10-22 16:20:15 -0300

    Add build

---
+++ build/precache-manifest.ddfbcb75478f6f4653c39a6f574a5d47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8567b29b3caa1151003ebd53b733f32a",
+    "url": "/projects/pattern-extractor/index.html"
+  },
+  {
+    "revision": "f522702284aeb54190a8",
+    "url": "/projects/pattern-extractor/static/css/main.53f3a60e.chunk.css"
+  },
+  {
+    "revision": "a8a5b0819b3b2419727d",
+    "url": "/projects/pattern-extractor/static/js/2.ec514d93.chunk.js"
+  },
+  {
+    "revision": "f522702284aeb54190a8",
+    "url": "/projects/pattern-extractor/static/js/main.97685939.chunk.js"
+  },
+  {
+    "revision": "2b483624be97c71d245f",
+    "url": "/projects/pattern-extractor/static/js/runtime-main.6c3db43f.js"
+  },
+  {
+    "revision": "4ba0aac161201dfe9036a293a8a1f4e3",
+    "url": "/projects/pattern-extractor/static/media/hero.4ba0aac1.png"
+  }
+]);
